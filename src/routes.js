c81f8ee98672a02ab7d38f58c6d56cd7c12b11fe
EnriqueB93 <enriquejblancob93@gmailcom>
2024-11-27 07:21:04 -0300

    update

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,7 @@
-import { Router } from "express"
+import { Router } from "express";
 
-const routes = new Router()
+const routes = new Router();
 
+routes.get("/", (req, res) => res.status(200).json({ message: " Hello Word" }));
 
-routes.get("/", (req, res) => {
-    return res.status(200).json({ message: " Hello Word" })
-
-})
-
-export default  routes
+export default routes;
