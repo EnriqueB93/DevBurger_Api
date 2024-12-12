03bb295462f7f3dc5e8c44eb3638a3c6647a1649
EnriqueB93 <enriquejblancob93@gmailcom>
2024-12-11 21:18:46 -0300

    config-controller

--- src/database/migrations/20241129091503-create-users-table.js
+++ src/database/migrations/20241129091503-create-users-table.js
@@ -28,12 +28,12 @@ module.exports = {
 				type: Sequelize.BOOLEAN,
 				defaultValue: false,
 			},
-			create_at: {
+			created_at: {
 				type: Sequelize.DATE,
 				allowNull: false,
 			},
 
-			update_at: {
+			updated_at: {
 				type: Sequelize.DATE,
 				allowNull: false,
 			},
