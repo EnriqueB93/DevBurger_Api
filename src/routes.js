5e27af0c6c3588e8b094889f8b24c76a084800f4
EnriqueB93 <enriquejblancob93@gmailcom>
2024-12-25 13:22:37 -0300

    create-ProductsController-and-routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import ProductsController from './app/controllers/ProductsController';
+import SessionController from './app/controllers/SessionConhtroller';
 import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store);
+routes.post('/products', ProductsController.store);
 
 export default routes;
